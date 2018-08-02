71228b32b34800da2227911d2449496c93cb24a3
RajaRaviVarma <ravi.varma@cybrilla.com>
2018-08-02 22:08:36 +0530

    Add duplicates attributes to ImageFile class

--- image_viewer/image_list_model.py
+++ image_viewer/image_list_model.py
@@ -12,9 +12,14 @@ from PyQt5.QtCore import (
 
 
 class ImageFile:
-    def __init__(self, path):
+    def __init__(self, path, signature=None, duplicates=None):
         self.path = path
         self.name = os.path.basename(path)
+        self.signature = signature
+        self.duplicates = duplicates or []
+
+    def add_duplicate(self, duplicate_image):
+        self.duplicates.append(duplicate_image)
 
 
 class ImageListModel(QAbstractListModel):
